f56b12bb4d48cc1f0668ae3895a4fc8c47f7ff4d
aaron atmaca <50340279+erenat77@users.noreply.github.com>
2019-10-08 15:23:22 -0700

    Update fraction.py
    added new definitions

--- fraction.py
+++ fraction.py
@@ -1,15 +1,32 @@
-class Fraction:
-  def __init__(self,top,bottom):
-        self.num = top
-        self.den = bottom  
-        
-  def __add__(self,otherfraction):
-     newnum = self.num*otherfraction.den + self.den*otherfraction.num
-     newden = self.den * otherfraction.den
-     return Fraction(newnum,newden)
+def gcd(m,n):
+    while m%n != 0:
+        oldm = m
+        oldn = n
 
-  def __str__(self):
-    return str(self.num)+"/"+str(self.den)
-    
-  def show(self):
-    print(self.num,"/",self.den)
+        m = oldn
+        n = oldm%oldn
+    return n
+
+class Fraction:
+     def __init__(self,top,bottom):
+         self.num = top
+         self.den = bottom
+
+     def __str__(self):
+         return str(self.num)+"/"+str(self.den)
+
+     def show(self):
+         print(self.num,"/",self.den)
+
+     def __add__(self,otherfraction):
+         newnum = self.num*otherfraction.den + \
+                      self.den*otherfraction.num
+         newden = self.den * otherfraction.den
+         common = gcd(newnum,newden)
+         return Fraction(newnum//common,newden//common)
+
+     def __eq__(self, other):
+         firstnum = self.num * other.den
+         secondnum = other.num * self.den
+
+         return firstnum == secondnum
